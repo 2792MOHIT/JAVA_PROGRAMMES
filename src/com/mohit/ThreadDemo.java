f7e893e1b801e53d27148ea14898cff70cb57ad8
MOHIT ARYA <2792mohit@gmail.com>
2020-04-16 13:41:09 +0530

    ThreadDemo

---
+++ src/com/mohit/ThreadDemo.java
@@ -0,0 +1,21 @@
+package com.mohit;
+
+class MyThread extends Thread {
+    @Override
+    public void run() {
+        super.run();
+        for (int i = 0;i < 10;i++)
+        System.out.println("CHILD THREAD");
+    }
+}
+
+public class ThreadDemo {
+    public static void main(String[] args) {
+
+        MyThread myThread = new MyThread();
+        myThread.start();
+
+        for(int i = 0; i < 10; i++)
+            System.out.println("MAIN THREAD");
+    }
+}
