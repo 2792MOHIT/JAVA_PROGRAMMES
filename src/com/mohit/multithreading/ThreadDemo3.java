d20943dbc4e623f4f22eb993d320eb7118187112
MOHIT ARYA <2792mohit@gmail.com>
2020-04-17 00:49:11 +0530

    adding ThreadDemo3

---
+++ src/com/mohit/multithreading/ThreadDemo3.java
@@ -0,0 +1,14 @@
+package com.mohit.multithreading;
+
+
+
+class MyThread3 extends Thread {
+
+}
+
+public class ThreadDemo3 {
+    public static void main(String[] args) {
+        MyThread3 t = new MyThread3();
+        t.start();
+    }
+}
