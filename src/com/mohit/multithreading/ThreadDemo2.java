48c17b6f494673d928f6f8b5e459e0fd290c5bab
MOHIT ARYA <2792mohit@gmail.com>
2020-04-17 00:15:33 +0530

    adding ThreadDemo2

---
+++ src/com/mohit/multithreading/ThreadDemo2.java
@@ -0,0 +1,19 @@
+package com.mohit.multithreading;
+
+class MyThread2 extends Thread {
+    @Override
+    public void run() {
+        System.out.println("no-arg run");
+    }
+
+    public void run(int i) {
+        System.out.println("int-arg run");
+    }
+}
+
+public class ThreadDemo2 {
+    public static void main(String[] args) {
+        MyThread2 t = new MyThread2();
+            t.start();
+    }
+}
