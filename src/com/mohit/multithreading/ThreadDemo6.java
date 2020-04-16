a15eec224338cd38d2c423d2538df06cc1966803
MOHIT ARYA <2792mohit@gmail.com>
2020-04-17 02:36:24 +0530

    adding ThreadDemo6

---
+++ src/com/mohit/multithreading/ThreadDemo6.java
@@ -0,0 +1,21 @@
+package com.mohit.multithreading;
+
+class MyThread6 extends Thread {
+
+    public void start() {
+        super.start();
+        System.out.println("start method");
+    }
+
+    public void run(){
+        System.out.println("run method");
+    }
+}
+public class ThreadDemo6 {
+    public static void main(String[] args) {
+        MyThread6 t = new MyThread6();
+        t.start();
+        System.out.println("main method");
+        t.start();
+    }
+}
